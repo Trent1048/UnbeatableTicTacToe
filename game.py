4f80f33cb7652b232a0d1cb615d97e752012e2a2
Trent Bultsma <34245971+Trent1048@users.noreply.github.com>
2019-02-09 06:20:41 -0800

    add display board to show how number input corresponds to spot on board

--- game.py
+++ game.py
@@ -8,7 +8,42 @@ noList = ["no", "No", "n", "N"]
 
 print("Welcome to Unbeatable Tic Tac Toe, you will not win \n")
 while notQuit:
+    board = Board()from player import Player
+from board import Board
+from bot import Bot
+
+notLost = True
+notQuit = True
+noList = ["no", "No", "n", "N"]
+displayBoard = Board(True)
+
+print("Welcome to Unbeatable Tic Tac Toe, you will not win \nPress the number in the corresponding square to put an X in it\n")
+displayBoard.display()
+while notQuit:
     board = Board()
+    gameboard = board.board
+    player1 = Player("X", gameboard)
+    bot = Bot("O", board)
+    while notLost: #main game loop
+        board.display()
+        while True: #make sure input is good loop
+            playerInput = input("Choose a spot on the num pad to put an X: ")
+            if playerInput.isdigit() and player1.validLocation(int(playerInput)):
+                break
+            else:
+                print("INVALID INPUT")
+        print()
+        board.turns += 1
+        player1.turn(int(playerInput))
+        bot.turn()
+        notLost = not board.testWin()
+    board.display()
+    if input("Want to try again? ") in noList:
+        notQuit = False
+    else:
+        notLost = True
+        print()
+print("\nBye now ")
     gameboard = board.board
     player1 = Player("X", gameboard)
     bot = Bot("O", board)
